664f32985ee127e98e649d844770771f1a776eaa
Bodhi Mulders <info@bemacized.net>
2017-08-11 15:22:01 +0200

    Disable help command temporarily

--- src/main/java/net/bemacized/grimoire/commands/HelpCommand.java
+++ src/main/java/net/bemacized/grimoire/commands/HelpCommand.java
@@ -37,40 +37,7 @@ public class HelpCommand extends BaseCommand {
 
 	@Override
 	public void exec(String[] args, MessageReceivedEvent e) {
-		// Construct message
-		final StringBuilder sb = new StringBuilder();
-		// Add header
-		sb.append("Hi there! This is the help page for Mac's Grimoire.").append("\n");
-		sb.append("Below, you will find all usable commands with what they do and how to use them.").append("\n");
-		sb.append("You can use both `/` or `!` as the command prefix.").append("\n");
-		sb.append("\n");
-
-		// Add command data
-		this.getCommandClasses().forEach(cmd -> {
-			try {
-				BaseCommand c = cmd.getDeclaredConstructor().newInstance();
-				sb.append(String.format("**!%s** %s", c.name(), c.paramUsage())).append("\n");
-				if (c.aliases() != null && c.aliases().length > 0)
-					sb.append(String.format("**Aliases:** _%s_", String.join(", ", c.aliases()))).append("\n");
-				sb.append(c.description()).append("\n");
-				sb.append("\n");
-			} catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException ex) {
-				LOG.log(Level.SEVERE, "An error occurred listing the help text", ex);
-			}
-		});
-
-		// Add footer
-		sb.append("Mac's Grimoire is being developed by BeMacized(<https://bemacized.net/>)").append("\n");
-		sb.append("\n");
-		sb.append("You can find the source code over at GitHub: <https://github.com/BeMacized/Grimoire>").append("\n");
-		sb.append("Contributors are always welcome!").append("\n");
-		sb.append("For feature suggestions or bug reports, please submit an issue with the issue tracker: <https://github.com/BeMacized/Grimoire/issues>").append("\n");
-		sb.append("").append("\n");
-		sb.append("In case you have any questions regarding Mac's Grimoire, feel free to contact my developer").append("\n");
-		sb.append("via Twitter: <https://twitter.com/BeMacized>").append("\n");
-		sb.append("or via e-mail: info@bemacized.net").append("\n");
-
-		e.getChannel().sendMessage(sb.toString()).submit();
+		sendEmbed(e.getChannel(), ":worried: The help command is currently being rewritten. Please try again later!");
 	}
 
 	private List<Class<? extends BaseCommand>> getCommandClasses() {
