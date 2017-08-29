85a8a815db4c237cac3cbb5a1abb935cc694c6c1
Bodhi Mulders <info@bemacized.net>
2017-08-30 00:51:01 +0200

    Opt-in embed merge

--- src/main/java/net/bemacized/grimoire/commands/all/CardCommand.java
+++ src/main/java/net/bemacized/grimoire/commands/all/CardCommand.java
@@ -5,7 +5,13 @@ import net.bemacized.grimoire.commands.CardBaseCommand;
 import net.bemacized.grimoire.data.models.card.MtgCard;
 import net.bemacized.grimoire.data.models.preferences.GuildPreferences;
 import net.bemacized.grimoire.utils.LoadMessage;
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.entities.MessageEmbed;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class CardCommand extends CardBaseCommand {
 
@@ -34,11 +40,25 @@ public class CardCommand extends CardBaseCommand {
 		// Update load text
 		loadMsg.setLineFormat("Loading card '%s' from set '%s, (%s)'...", card.getName(), card.getSet().getName(), card.getSet().getCode());
 
-		// Build embed & show
-		loadMsg.complete(card.getEmbed(e.getGuild(), guildPreferences));
+		// Get card embed
+		EmbedBuilder eb = new EmbedBuilder(card.getEmbed(e.getGuild(), guildPreferences));
 
-		// Queue price if needed
-		if (guildPreferences.showPriceOnCard())
-			e.getChannel().sendMessage(Grimoire.getInstance().getPricingProvider().getPricingEmbed(card, guildPreferences)).queue();
+		// Inject prices if preferred
+		if (guildPreferences.showPriceOnCard()) {
+			final  MessageEmbed pricing = Grimoire.getInstance().getPricingProvider().getPricingEmbed(card, guildPreferences);
+			String descLine = pricing.getDescription().split("\n")[0];
+			final String set = (descLine.matches(".*?[(].+?[)]"))
+					? descLine.substring(descLine.indexOf("(") + 1, descLine.indexOf(")", descLine.indexOf("(")))
+					: "";
+			boolean showSet = !set.equalsIgnoreCase(card.getSet().getCode()) && !set.isEmpty();
+			pricing.getFields().forEach(f -> {
+				List<String> desc = Stream.of(f.getValue().split("\n")).collect(Collectors.toList());
+				desc.set(0, desc.get(0) + (showSet ? " _(" + set + ")_" : ""));
+				eb.addField(f.getName(), String.join("\n", desc), f.isInline());
+			});
+		}
+
+		// Build embed & show
+		loadMsg.complete(eb.build());
 	}
 }
